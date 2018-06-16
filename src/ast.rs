1548f0d7c72e2afdb0fe49f549947e5f9271b999
Noam Baraz <n0987b@gmail.com>
2018-06-16 22:30:50 +0300

    Extend grammar

--- src/ast.rs
+++ src/ast.rs
@@ -1,11 +1,19 @@
 
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Func {
+    pub name: Ident,
+    pub params: Vec<Ident>,
+    pub body: Block,
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Item {
-    Func {
+    Func(Func),
+    Type {
         name: Ident,
-        params: Vec<Ident>,
-        body: Block,
-    }
+        methods: Vec<Func>
+    },
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -20,11 +28,26 @@ pub enum Expr {
     Ident(Ident),
     BinOp(Box<Expr>, BinOp, Box<Expr>),
     Block(Block),
+    Assign(Ident, Box<Expr>),
+    AttrLookup(Box<Expr>, Ident),
+    Call(Box<Expr>, Vec<Expr>),
 }
 
 impl Expr {
     pub fn new_op(a: Expr, op: BinOp, b: Expr) -> Expr {
         Expr::BinOp(Box::new(a), op, Box::new(b))
+    }
+
+    pub fn new_assign(ident: Ident, expr: Expr) -> Expr {
+        Expr::Assign(ident, Box::new(expr))
+    }
+
+    pub fn new_attrlookup(expr: Expr, ident: Ident) -> Expr {
+        Expr::AttrLookup(Box::new(expr), ident)
+    }
+
+    pub fn new_call(func: Expr, args: Vec<Expr>) -> Expr {
+        Expr::Call(Box::new(func), args)
     }
 }
 
