8b18ba28d7486c6f8c7a96b28a721fe382756117
Noam Baraz <n0987b@gmail.com>
2018-08-10 02:49:49 +0300

    wip codegen

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,7 +1,7 @@
 use std::iter::Extend;
 
-use instruction::Instruction;
 use ast;
+use instruction::Instruction;
 
 struct Function {
     name: String,
@@ -14,13 +14,20 @@ struct Type {
     methods: Vec<Function>,
 }
 
-fn generate_from_expr<E: Extend<Function>>(expr: &ast::Expr, out: &mut E, use_result: bool) -> Vec<Instruction>
-{
+fn generate_from_expr<E: Extend<Function>>(
+    expr: &ast::Expr,
+    out: &mut E,
+    use_result: bool,
+) -> Vec<Instruction> {
     use ast::Expr::*;
-    let (mut insns, pushes_value) =  match *expr {
-        Num(n) => (vec![Instruction::CreateInt {value: n as i64}], true),
-        Ident(ast::Ident(ref name)) => (vec![Instruction::LookupName {name: name.clone()}], true),
-        _ => (vec![], false)
+    let (mut insns, pushes_value) = match *expr {
+        Num(n) => (vec![Instruction::CreateInt { value: n as i64 }], true),
+        Ident(ast::Ident(ref name)) => (vec![Instruction::LookupName { name: name.clone() }], true),
+        BinOp(ref a, op, ref b) => (generate_binop(&a, op, &b, out, use_result), use_result),
+        Block(ref block) => (generate_from_block(block, out, use_result), use_result),
+        Assign(ref ident, ref expr) => (generate_assign(&ident, &expr, out), false),
+        AttrLookup(ref expr, ref ident) => (generate_assign(&ident, &expr, out), false),
+        _ => (vec![], false),
     };
 
     if pushes_value && !use_result {
@@ -30,8 +37,59 @@ fn generate_from_expr<E: Extend<Function>>(expr: &ast::Expr, out: &mut E, use_re
     insns
 }
 
-fn generate_from_block<E: Extend<Function>>(block: &ast::Block, out: &mut E, use_result: bool) -> Vec<Instruction>
-{
+fn generate_binop<E: Extend<Function>>(
+    a: &ast::Expr,
+    op: ast::BinOp,
+    b: &ast::Expr,
+    out: &mut E,
+    use_result: bool,
+) -> Vec<Instruction> {
+    let mut v = vec![];
+    v.extend(generate_from_expr(a, out, true));
+    v.extend(generate_from_expr(b, out, true));
+    v.push(match op {
+        ast::BinOp::Add => Instruction::CallMethod {
+            name: "add".to_owned(),
+            num_args: 1,
+            use_result,
+        },
+        _ => unreachable!(),
+    });
+    v
+}
+
+fn generate_assign<E: Extend<Function>>(
+    ident: &ast::Ident,
+    expr: &ast::Expr,
+    out: &mut E,
+) -> Vec<Instruction> {
+    use instruction::Instruction::*;
+    let mut v = generate_from_expr(expr, out, true);
+    v.push(Assign {
+        name: ident.0.clone(),
+    });
+    v
+}
+
+fn generate_attrlookup<E: Extend<Function>>(
+    expr: &ast::Expr,
+    ident: &ast::Ident,
+    out: &mut E,
+    use_result: bool,
+) -> Vec<Instruction> {
+    use instruction::Instruction::*;
+    let mut v = generate_from_expr(expr, out, true);
+    v.push(GetMember {
+        name: ident.0.clone(),
+    });
+    v
+}
+
+fn generate_from_block<E: Extend<Function>>(
+    block: &ast::Block,
+    out: &mut E,
+    use_result: bool,
+) -> Vec<Instruction> {
     let len = block.0.len();
     let mut insns = vec![];
     for (idx, expr) in block.0.iter().enumerate() {
@@ -40,11 +98,9 @@ fn generate_from_block<E: Extend<Function>>(block: &ast::Block, out: &mut E, use
     }
 
     insns
-
 }
 
 // TODO: error handling
 fn generate_function<E: Extend<Function>>(func: ast::Func, out: &mut E) {
     generate_from_block(&func.body, out, true);
 }
-
